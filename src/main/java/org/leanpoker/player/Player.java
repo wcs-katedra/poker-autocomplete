f9eb117dcb10ba0fecbccb070f0f0b85e6e1450d
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-03-14 12:50:48 +0100

    fv kész Mennyi kártya ment el az osztótól ?

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,33 @@ public class Player {
 
     static final String VERSION = "Default Java folding player";
 
+    /**
+     * mennyi kártya ment el az osztótol ?
+     *
+     * @return db ra
+     */
+    public static Integer getNumberOfCardInTheGame(GameState gameState) {
+        Integer cards = 0;
+
+        for (com.wcs.poker.gamestate.Player player : gameState.getPlayers()) {
+            for (com.wcs.poker.gamestate.Card card : player.getHoleCards()) {
+                if (card != null) {
+                    cards++;
+                }
+            }
+        }
+        for (com.wcs.poker.gamestate.Card card : gameState.getCommunityCards()) {
+            if (card != null) {
+                cards++;
+            }
+        }
+
+        return cards;
+    }
+
     public static int betRequest(GameState gameState) {
+        Integer cardsOut = getNumberOfCardInTheGame(gameState);
+
         return 0;
     }
 
