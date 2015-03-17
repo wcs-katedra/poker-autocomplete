93c252b43f41f1a33b1c720317fec3fb248043dd
BalazsMartonZoltan <sportcsoki@gmail.com>
2015-03-17 22:42:13 +0100

    #6  RankCount & SuitCount
    A két osztály létrehozása, megírása

--- src/main/java/org/leanpoker/player/CardAnalysis.java
+++ src/main/java/org/leanpoker/player/CardAnalysis.java
@@ -12,3 +12,54 @@ package org.leanpoker.player;
 public class CardAnalysis {
     
 }
+
+class RankCount{
+    
+   private String rank;
+   private Integer count;
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+   
+   
+
+}
+
+
+class SuitCount{
+    
+    private String suit;
+    private Integer count;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+    
+    
+    
+}
