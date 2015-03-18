516bed18b5ccceb626377d088ee88b7ecd780230
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-03-18 19:39:02 +0100

    make test --- it runs correct

--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,6 +1,9 @@
 package org.leanpoker.player;
 
+import com.wcs.poker.gamestate.Card;
 import com.wcs.poker.gamestate.GameState;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,10 +11,21 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
 
     @Test
-    public void testBetRequest() throws Exception {
-        final GameState gameState = new GameState();
-
-        assertEquals(0, Player.betRequest(gameState));
+    public void testgetNumberOfCardInTheGame() throws Exception {
+        GameState gs = new GameState();
+        
+        List<Card> cards = new ArrayList();
+        Card card = new Card();
+        
+        cards.add(card);
+        cards.add(card);
+        cards.add(card);
+        
+        gs.setCommunityCards(cards);
+        
+        Player player = new Player();
+        assertEquals(new Integer(3), player.getNumberOfCardInTheGame(gs));
 
     }
+
 }
