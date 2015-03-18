9852e66f0fe1e1423caa67f2b12cf3d02bf8e3a7
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-03-18 18:00:40 +0100

    thinking ....

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,7 +4,7 @@ import com.wcs.poker.gamestate.GameState;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Default+ Java folding player";
 
     /**
      * mennyi kártya ment el az osztótol ?
@@ -14,13 +14,11 @@ public class Player {
     public static Integer getNumberOfCardInTheGame(GameState gameState) {
         Integer cards = 0;
 
+        // a járékosoknál lévő lapok száma
         for (com.wcs.poker.gamestate.Player player : gameState.getPlayers()) {
-            for (com.wcs.poker.gamestate.Card card : player.getHoleCards()) {
-                if (card != null) {
-                    cards++;
-                }
-            }
+            
         }
+        // az asztalon lévő lapok száma
         for (com.wcs.poker.gamestate.Card card : gameState.getCommunityCards()) {
             if (card != null) {
                 cards++;
@@ -30,6 +28,13 @@ public class Player {
         return cards;
     }
 
+    /**
+     * 
+     * @param gameState
+     * @return 0 -- ha bedobod
+     *         ALL> -- aktuális tét
+     *          ALL -- minden benn
+     */
     public static int betRequest(GameState gameState) {
         Integer cardsOut = getNumberOfCardInTheGame(gameState);
 
