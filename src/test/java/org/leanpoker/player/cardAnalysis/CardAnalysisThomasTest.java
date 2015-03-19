fce791a16392b0b3b0381b12b42607d5012a3d2c
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-03-19 21:32:21 +0100

    do nothing

--- src/test/java/org/leanpoker/player/cardAnalysis/CardAnalysisThomasTest.java
+++ src/test/java/org/leanpoker/player/cardAnalysis/CardAnalysisThomasTest.java
@@ -15,7 +15,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.leanpoker.player.cardAnalysis.enums.CardClass;
-import org.leanpoker.player.cardAnalysis.enums.CardsValue;
 
 /**
  *
