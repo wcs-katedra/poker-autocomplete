17d5d072b73474c7154e2a830f78ae23051ab80c
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-04-04 22:59:00 +0200

    genereta random car what not exists in the game jet

---
+++ src/test/java/org/leanpoker/player/RandomCard.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.leanpoker.player;
+
+import com.wcs.poker.gamestate.Card;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ *
+ * @author boss
+ */
+public class RandomCard {
+
+    private Set<Card> sentCards = new HashSet<Card>();
+    private final String[] ranks = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
+    private final String[] suits = {"heart", "diamond", "spade", "asd"};
+
+    
+    /**
+     * Genereta a random card. Returns it only if it was not sended out earlyer
+     * @return a random card
+     */
+    public Card getRandomCard() {
+        Random rnd = new Random();
+        int rndRank;
+        int rndSuit;
+        Card card = new Card();
+        
+        do{           
+            rndRank = rnd.nextInt(ranks.length);
+            card.setRank(ranks[rndRank]);
+            
+            rndSuit = rnd.nextInt(suits.length);
+            card.setSuit(suits[rndSuit]);
+        }while(sentCards.contains(card));
+        sentCards.add(card);
+
+        return card;
+    }
+
+}
