b7288e30401364d2a2c303a58697c75ed51e99fd
BalazsMartonZoltan <sportcsoki@gmail.com>
2015-04-24 12:18:37 +0200

    #10 level beépítése a Hand-be

--- src/main/java/com/wcs/poker/hand/work/Hand.java
+++ src/main/java/com/wcs/poker/hand/work/Hand.java
@@ -3,6 +3,7 @@ package com.wcs.poker.hand.work;
 
 
 import com.wcs.poker.gamestate.Card;
+import com.wcs.poker.hand.enums.HandLevel;
 import com.wcs.poker.hand.enums.HandRank;
 import java.util.Collection;
 import java.util.Collections;
@@ -15,23 +16,33 @@ import java.util.List;
 public class Hand {
 
     private final HandRank rank;
+    private final HandLevel level;
     private final Collection<Card> cards;
 
-    public Hand(HandRank rank, List<Card> cards) {
+    
+     public Hand(HandRank rank,HandLevel level, List<Card> cards) {
         this.rank = rank;
+        this.level=level;
         this.cards = cards;
     }
 
     @Override
     public String toString() {
-        return "Hand{" + "rank=" + rank + ", cards=" + cards + '}';
+        return "Hand{" + "rank=" + rank + ", level=" + level + ", cards=" + cards + '}';
     }
+
+   
 
     
     public HandRank getRank() {
         return rank;
     }
 
+    public HandLevel getLevel() {
+        return level;
+    }
+
+    
     public Collection<Card> getCards() {
         return cards;
     }
--- src/main/java/com/wcs/poker/hand/work/HandRankingService.java
+++ src/main/java/com/wcs/poker/hand/work/HandRankingService.java
@@ -1,8 +1,7 @@
 package com.wcs.poker.hand.work;
 
-
-
 import com.wcs.poker.gamestate.Card;
+import com.wcs.poker.hand.enums.HandLevel;
 import com.wcs.poker.hand.enums.HandRank;
 import java.util.Collections;
 import java.util.List;
@@ -13,20 +12,22 @@ import java.util.List;
  */
 public class HandRankingService {
 
-    
     public Hand evaulate(List<Card> loadCards) {
-        if (loadCards.size()<2 || loadCards.size()>7){
+        if (loadCards.size() < 2 || loadCards.size() > 7) {
             throw new IllegalArgumentException();
         }
-        FinalResult finalResult = new FinalResult();        
+        FinalResult finalResult = new FinalResult();
+
         finalResult.evaluateCards(loadCards);
-        
+
         HandRank rank = finalResult.getCombo();
-        Collections.sort(loadCards);
-        
-        Hand hand = new Hand(rank, loadCards);
-        
+        HandLevel level = finalResult.getLevel();
+
+        Hand hand;
+
+        hand = new Hand(rank, level, loadCards);
+
         return hand;
     }
-    
+
 }
